c8877b907c7bf186438a0cf209b4602e11fa102c
Stanislav Protasevich <stanislav.protasevich@gmail.com>
2018-12-10 19:36:25 +0200

    Fixed bug

--- config/router.php
+++ config/router.php
@@ -5,5 +5,5 @@ $router = $routerContainer->getMap();
 
 $router->get('home', '/', \NtSchool\Action\HomeAction::class);
 $router->get('blog', '/blog', \NtSchool\Action\HomeAction::class);
-$router->get('posts', '/posts', \NtSchool\Action\HomeAction::class);
+$router->get('post', '/post', \NtSchool\Action\HomeAction::class);
 $router->get('cat', '/category/{id}', \NtSchool\Action\PostByCategoryAction::class);
