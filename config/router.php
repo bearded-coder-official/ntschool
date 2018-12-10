162f198d0516f315c6d9d7a24b0f62a754a9199b
Stanislav Protasevich <stanislav.protasevich@gmail.com>
2018-12-10 19:28:20 +0200

    Добавил роут постов

--- config/router.php
+++ config/router.php
@@ -5,4 +5,5 @@ $router = $routerContainer->getMap();
 
 $router->get('home', '/', \NtSchool\Action\HomeAction::class);
 $router->get('blog', '/blog', \NtSchool\Action\HomeAction::class);
+$router->get('posts', '/posts', \NtSchool\Action\HomeAction::class);
 $router->get('cat', '/category/{id}', \NtSchool\Action\PostByCategoryAction::class);
